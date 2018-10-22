67f26869783f863941fe2e9ffffcbf77102a035d
Swagata Chakraborty <cswagata@IN-swagata.local>
2018-10-22 13:22:19 +0530

    serial number is corrected.

--- testFrameWork.js
+++ testFrameWork.js
@@ -17,29 +17,34 @@ const counter = function(count) {
   }
 }
 
-const count = counter(0);
+const count = counter(1);
 const repeatSpace = repeat(" ");
 const repeatDash = repeat("-");
 
-const createHeader = function() {
+const createBorder = function() {
   let border = repeatDash(170) ;
   console.log(border);
+}
+
+const createHeader = function() {
   let functionHeader = "   | " + leftJustifier("FunctionName",30) + " | ";
   functionHeader += leftJustifier("Inputs",30) + " | ";
   functionHeader += leftJustifier("Actual Output",50) + " | ";
   functionHeader += leftJustifier("Expected Output",50) ;
+  createBorder();
   console.log(functionHeader);
-  console.log(border);
+  createBorder();
 }
 
 const generateTestReports = function(func, inputs, actualOutput, expectedOutput) {
-  if( count() == 0) { 
+  let serial = count();
+  if( serial == 1) { 
     createHeader();
   }
   let functionTest = leftJustifier(JSON.stringify(func),30);
   let givenInputs = leftJustifier(JSON.stringify(inputs),30);
   let output = leftJustifier(JSON.stringify(actualOutput),50);
-  console.log("",count(),"|",functionTest,"|",givenInputs,"|",output,"|",expectedOutput);
+  console.log("",serial,"|",functionTest,"|",givenInputs,"|",output,"|",expectedOutput);
 }
 
 const leftJustifier = function(text,width) {
