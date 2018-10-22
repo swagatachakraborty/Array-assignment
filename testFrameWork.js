d0cd40701b5ba425d3d2fcda5ce598cd65a9b016
Swagata Chakraborty <cswagata@IN-swagata.local>
2018-10-22 13:14:09 +0530

    Header and serial number is added to the test report.

--- testFrameWork.js
+++ testFrameWork.js
@@ -5,18 +5,45 @@ const checkAssert = function(func, inputs, actualOutput, expectedOutput, errorMe
   assert.deepEqual(expectedOutput,actualOutput,errorMessage);
 }
 
+const repeat = function(char) {
+ return repeatChar = function(times) {
+  return new Array(times).fill(char).join(""); 
+ }
+}
+
+const counter = function(count) {
+  return function () {
+    return count++;
+  }
+}
+
+const count = counter(0);
+const repeatSpace = repeat(" ");
+const repeatDash = repeat("-");
+
+const createHeader = function() {
+  let border = repeatDash(170) ;
+  console.log(border);
+  let functionHeader = "   | " + leftJustifier("FunctionName",30) + " | ";
+  functionHeader += leftJustifier("Inputs",30) + " | ";
+  functionHeader += leftJustifier("Actual Output",50) + " | ";
+  functionHeader += leftJustifier("Expected Output",50) ;
+  console.log(functionHeader);
+  console.log(border);
+}
+
 const generateTestReports = function(func, inputs, actualOutput, expectedOutput) {
+  if( count() == 0) { 
+    createHeader();
+  }
   let functionTest = leftJustifier(JSON.stringify(func),30);
   let givenInputs = leftJustifier(JSON.stringify(inputs),30);
   let output = leftJustifier(JSON.stringify(actualOutput),50);
-  console.log("|",functionTest,"|",givenInputs,"|",output,"|",expectedOutput);
+  console.log("",count(),"|",functionTest,"|",givenInputs,"|",output,"|",expectedOutput);
 }
 
 const leftJustifier = function(text,width) {
   return text+repeatSpace(width - text.length); 
 }
 
-const repeatSpace = function(times) {
-  return new Array(times).fill(" ").join(""); 
-}
 exports.checkAssert = checkAssert;
