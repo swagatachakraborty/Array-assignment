5e11aabdd9f0e147b5d59a5feb4493b9f4107ca2
Swagata Chakraborty <cswagata@IN-swagata.local>
2018-10-22 12:25:18 +0530

    Output is well organised.

--- testFrameWork.js
+++ testFrameWork.js
@@ -6,7 +6,17 @@ const checkAssert = function(func, inputs, actualOutput, expectedOutput, errorMe
 }
 
 const generateTestReports = function(func, inputs, actualOutput, expectedOutput) {
-  console.log(func,"\t\t",inputs,"\t\t",actualOutput,"\t\t",expectedOutput);
+  let functionTest = leftJustifier(JSON.stringify(func),30);
+  let givenInputs = leftJustifier(JSON.stringify(inputs),30);
+  let output = leftJustifier(JSON.stringify(actualOutput),50);
+  console.log("|",functionTest,"|",givenInputs,"|",output,"|",expectedOutput);
 }
 
+const leftJustifier = function(text,width) {
+  return text+repeatSpace(width - text.length); 
+}
+
+const repeatSpace = function(times) {
+  return new Array(times).fill(" ").join(""); 
+}
 exports.checkAssert = checkAssert;
